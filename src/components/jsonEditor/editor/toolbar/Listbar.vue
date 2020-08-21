179aaa280800486740750742e88d2d2293f9b8b1
falstack <icesilt@outlook.com>
2020-08-21 23:11:46 +0800

    add: toolbar

---
+++ src/components/jsonEditor/editor/toolbar/Listbar.vue
@@ -0,0 +1,21 @@
+<style lang="scss">
+</style>
+
+<template>
+</template>
+
+<script>
+export default {
+  name: 'ListBar',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>

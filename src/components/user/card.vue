0e78e078fd47a0bba14535dee726777915bc4cc5
falstack <icesilt@outlook.com>
2021-05-17 09:38:51 +0800

    feat: add user card

---
+++ src/components/user/card.vue
@@ -0,0 +1,179 @@
+<template>
+  <view class="user-card">
+    <view class="header">
+      <view class="nickname">
+        {{ item.nickname }}
+      </view>
+      <view
+        v-if="item.meta.sex !== null"
+        class="sex"
+      >
+        <view
+          v-if="item.meta.sex === 0"
+          class="iconfont ic-nv"
+        />
+        <view
+          v-else
+          class="iconfont ic-nan"
+        />
+      </view>
+      <view
+        v-if="item.realname"
+        class="iconfont ic-shiming"
+      />
+    </view>
+    <view class="body">
+      <view class="left">
+        {{ item.aboutus.aboutme }}
+      </view>
+      <view class="right">
+        <image :src="item.avatar" />
+      </view>
+    </view>
+    <view class="footer">
+      <view class="item location">
+        <view
+          v-if="item.meta.location"
+          class="iconfont ic-weizhi"
+        />
+        <view class="name">
+          {{ item.meta.location }}
+        </view>
+      </view>
+      <view
+        v-if="item.meta.birthday"
+        class="item age"
+      >
+        <view class="iconfont ic-nianling" />
+        <view class="name">
+          {{ item.meta.birthday }}
+        </view>
+      </view>
+      <view
+        v-if="item.meta.bodyHeight"
+        class="item height"
+      >
+        <view class="iconfont ic-shengao" />
+        <view class="name">
+          {{ item.meta.bodyHeight }}cm
+        </view>
+      </view>
+      <view
+        v-if="item.meta.bodyWeight"
+        class="item weight"
+      >
+        <view class="iconfont ic-weight" />
+        <view class="name">
+          {{ item.meta.bodyWeight }}kg
+        </view>
+      </view>
+      <view
+        v-if="item.meta.work"
+        class="item work"
+      >
+        <view class="iconfont ic-gongzuo" />
+        <view class="name">
+          {{ item.meta.work }}
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'UserCard',
+  components: {},
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.user-card {
+  background-color: #fff;
+  padding: 15px;
+  margin-bottom: 20px;
+
+  .header {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    padding-bottom: 15px;
+    border-bottom: 1px solid rgba(0,0,0,.05);
+
+    .nickname {
+      @extend %oneline;
+    }
+
+    view {
+      margin-right: 6px;
+    }
+
+    .ic-nv {
+      color: #fb7299;
+    }
+
+    .ic-nan {
+      color: #00a1d6;
+    }
+
+    .ic-shiming {
+      color: goldenrod;
+    }
+  }
+
+  .body {
+    display: flex;
+    flex-direction: row;
+    height: 240px;
+    margin: 15px 0;
+
+    .left {
+      flex: 1;
+      padding-right: 15px;
+      text-indent: 2em;
+      color: #999;
+      @include mutiline(40px, 6);
+      @extend %breakWord;
+    }
+
+    .right {
+      flex-shrink: 0;
+
+      image {
+        width: 240px;
+        height: 240px;
+        border-radius: 10%;
+        overflow: hidden;
+      }
+    }
+  }
+
+  .footer {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    padding-top: 15px;
+    border-top: 1px solid rgba(0,0,0,.05);
+
+    .item {
+      margin-right: 15px;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .iconfont {
+      margin-right: 4px;
+    }
+  }
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,93 @@
 <template>
   <view>
     <Navbar />
-    <view>
-      index
+    <view class="listview">
+      <UserCard
+        v-for="item in state.result"
+        :key="item.slug"
+        :item="item"
+      />
     </view>
   </view>
 </template>
 
 <script>
+import http from '~/utils/http'
 import Navbar from '~/components/navbar'
+import UserCard from '~/components/user/card'
+import { reactive, onMounted } from 'vue'
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    UserCard
+  },
+  setup() {
+    const state = reactive({
+      result: [],
+      noMore: false,
+      nothing: false,
+      loading: false,
+      error: null,
+      fetched: false,
+    })
+
+    const initData = (refresh = false) => {
+      if (state.loading) {
+        return
+      }
+
+      if (!refresh) {
+        state.loading = true
+        state.error = null
+      }
+
+      http.get('user/about_flow')
+        .then(res => {
+          state.result = res.result
+          state.noMore = res.result.length < 20
+          state.fetched = true
+        })
+        .catch((err) => {
+          state.error = err
+        })
+        .finally(() => {
+          state.loading = false
+        })
+    }
+
+    const loadMore = () => {
+      if (state.loading || state.noMore) {
+        return
+      }
+
+      state.loading = true
+      state.error = null
+
+      http.get('user/about_flow', {
+        seen_ids: state.result.map(_ => _.id).join(',')
+      })
+        .then(res => {
+          state.result = res.result
+          state.noMore = res.result.length < 20
+          state.fetched = true
+        })
+        .catch((err) => {
+          state.error = err
+        })
+        .finally(() => {
+          state.loading = false
+        })
+    }
+
+    onMounted(() => {
+      initData()
+    })
+
+    return {
+      state,
+      loadMore
+    }
   },
   onShareAppMessage() {
     return {
